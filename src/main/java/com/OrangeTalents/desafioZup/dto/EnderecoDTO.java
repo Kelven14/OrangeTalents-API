51227608a9f596dd4413f1c0b84efa168ad58312
kelven14 <kelven.cleiton14@gmail.com>
2021-04-29 20:02:49 -0300

    Implementando o DTO( usuario/endereço)

---
+++ src/main/java/com/OrangeTalents/desafioZup/dto/EnderecoDTO.java
@@ -0,0 +1,124 @@
+package com.OrangeTalents.desafioZup.dto;
+
+import java.io.Serializable;
+
+import com.OrangeTalents.desafioZup.model.Endereco;
+import com.OrangeTalents.desafioZup.model.Usuario;
+
+public class EnderecoDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String logradouro;
+	private int numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	private Usuario usuario;
+
+	public EnderecoDTO() {
+	}
+
+	public EnderecoDTO(Long id, String logradouro, int numero, String complemento, String bairro, String cidade,
+			String estado, String cep, Usuario usuario) {
+		super();
+		this.id = id;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.cep = cep;
+		this.usuario = usuario;
+	}
+
+	public EnderecoDTO(Endereco entity) {
+		id = entity.getId();
+		logradouro = entity.getLogradouro();
+		numero = entity.getNumero();
+		complemento = entity.getComplemento();
+		bairro = entity.getBairro();
+		cidade = entity.getCidade();
+		estado = entity.getEstado();
+		cep = entity.getCep();
+		usuario = entity.getUsuario();
+	}
+
+	// Métodos Getters e Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
